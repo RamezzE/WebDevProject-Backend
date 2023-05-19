9d0af2fedef8962ff5e41e288d1bacf7c78c3360
Ramez Ehab <ramezehab2@gmail.com>
2023-05-19 14:05:24 +0300

    added admin func to delete users from dashboard

---
+++ public/JS/adminFunctions.js
@@ -0,0 +1,17 @@
+
+function deleteUser(userID) {
+    
+    var deleteForm = document.createElement('form');
+    deleteForm.method = 'POST';
+    deleteForm.action = 'dashboard/users/delete';
+
+    var userIdInput = document.createElement('input');
+    userIdInput.type = 'hidden';
+    userIdInput.name = 'userID';
+    userIdInput.value = userID;
+
+    deleteForm.appendChild(userIdInput);
+    document.body.appendChild(deleteForm);
+
+    deleteForm.submit();
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 import { Router } from 'express';
+import User from '../models/user.js';
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' })
 
@@ -8,14 +9,24 @@ var router = Router();
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const database = client.db('web11');
 const collection = database.collection('users');
-const users = await collection.find().toArray();
 
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res, next) => {
+  const users = await collection.find().toArray();
   res.render('dashboard', { users });
 });
 
-router.post('/', async (req, res) => {
-    
+router.post('/users/delete', async (req, res) => {
+  const { userID } = req.body;
+  console.log(userID);
+  const deletedUser = await User.findOneAndDelete({ _id: userID });
+
+  if (deletedUser)
+    console.log('User deleted')
+  else
+    console.log('User not found')
+
+  const users = await collection.find().toArray();
+  return res.redirect('/dashboard');
 });
 
 export default router;
