774c68ffde5abf2a2bedeaccddc7bc5c1e7f5058
Ramez Ehab <ramezehab2@gmail.com>
2023-05-21 20:13:33 +0300

    updated dashboard_users & added "add admin" button

--- public/JS/adminFunctions.js
+++ public/JS/adminFunctions.js
@@ -1,17 +1,26 @@
-
-function deleteUser(userID) {
-    
-    var deleteForm = document.createElement('form');
-    deleteForm.method = 'POST';
-    deleteForm.action = '/dashboard/users/delete';
-
-    var userIdInput = document.createElement('input');
-    userIdInput.type = 'hidden';
-    userIdInput.name = 'userID';
-    userIdInput.value = userID;
-
-    deleteForm.appendChild(userIdInput);
-    document.body.appendChild(deleteForm);
-
-    deleteForm.submit();
+function submitForm(field) {
+    let form = field.parentNode;
+    form.submit();
 }
+
+function copyToClipboard(button, className) {
+    console.log(className);
+    let parent = button.parentNode.parentNode; // Use parentNode instead of parent
+    let copyText = parent.querySelector('.' + className); // Use querySelector instead of getElementsByClassName and specify class with a dot prefix
+  
+    // Create a temporary textarea element
+    let tempTextarea = document.createElement('textarea');
+    tempTextarea.value = copyText.textContent.trim();
+    document.body.appendChild(tempTextarea);
+  
+    // Select the value inside the textarea
+    tempTextarea.select();
+    tempTextarea.setSelectionRange(0, 99999);
+    
+    // Execute the copy command
+    document.execCommand('copy');
+    
+    // Remove the temporary textarea
+    document.body.removeChild(tempTextarea);
+  }
+  
