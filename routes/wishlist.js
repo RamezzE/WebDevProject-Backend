51c7ec69ba2812bbae833273289c5c2f865e29e7
Kidzantso <116034195+Kidzantso@users.noreply.github.com>
2023-05-25 18:04:39 +0300

    working on cart and wishlist

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { MongoClient } from 'mongodb';
 var router = Router();
+import Product from '../models/product.js';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' })
+
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const database = client.db('web11');
+const productsCollection = database.collection('products');
 
 router.use((req, res, next) => {
   if (req.session.userType !== undefined) {
@@ -13,26 +21,34 @@ router.use((req, res, next) => {
   }
 });
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res, next)=> {
+  console.log(req.session.wishlist);
   let admin = false;
   if (req.session.userType == 'admin')
     admin = true;
-    const userData = {};
-  userData.wishlist = req.session.wishlist;
-  res.render('wishlist', { admin: admin ,userData});
+    let done=false;
+  req.session.wishlist.forEach(element => {
+  Product.find({ '_id': element})
+  .then(async products => {
+    console.log(products);
+    if(element===(req.session.wishlist[req.session.wishlist.size-1])){done=true;}
+    if(done){res.render('wishlist', { products: products, admin: admin });}})
+});
+ 
 });
 
 router.get('/:id', async (req, res, next) => {  
   console.log(req.session.wishlist);
   req.session.wishlist.push(req.params.id);
   console.log(req.session.wishlist);
-  const userData = {};
-  userData.wishlist = req.session.wishlist;
-    
+  let products =[];
+  req.session.wishlist.forEach(element => {
+    products.push(Product.findById(element));
+  });
     let admin = false;
   if (req.session.userType == 'admin')
     admin = true;
-  res.render('wishlist', {  admin: admin,userData });
+  res.render('wishlist', {  admin: admin,products: products });
 });
 
 export default router;
