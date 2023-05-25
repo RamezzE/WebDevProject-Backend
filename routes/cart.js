dc51c125a87cb0d46a256e251972ad76e869a9d7
Kidzantso <116034195+Kidzantso@users.noreply.github.com>
2023-05-25 03:17:17 +0300

    Confirm logging in to access

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,17 @@
 import { Router } from 'express';
 var router = Router();
 
+router.use((req, res, next) => {
+  if (req.session.userType !== undefined) {
+      next();
+  }
+  else {
+    let admin = false;
+  if (req.session.userType == 'admin')
+    admin = true;
+      res.render('err', { err: 'You must login to access this page',admin: admin })
+  }
+});
 /* GET home page. */
 router.get('/', function (req, res, next) {
   let admin = false;
