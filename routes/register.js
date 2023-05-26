d38519591ba976b57cabf289194731d7c0626a5b
aya <aya2103436@miuegypt.edu.eg>
2023-05-27 01:50:21 +0300

    Import user

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import User from '../models/user.js';
+
 var router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register');
 });
+
 
 export default router;
