865be11c3c94887b4625c5d939213dd986fa0b9e
nadaelamir101 <125657577+nadaelamir101@users.noreply.github.com>
2023-05-27 12:00:14 +0200

    login session

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+
 var router = Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login');
+router.get('/', function (req, res, next) {
+  if (req.session.userType)
+    return res.redirect('/');
+
+  let errorMsg = {};
+  res.render('login', { errorMsg });
 });
+
 
 export default router;
