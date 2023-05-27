1652659b55f8dc17ae468196c1a7600925c601de
nadaelamir101 <125657577+nadaelamir101@users.noreply.github.com>
2023-05-27 12:02:44 +0200

    login

--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,26 @@ router.get('/', function (req, res, next) {
   let errorMsg = {};
   res.render('login', { errorMsg });
 });
+router.post('/', async (req, res) => {
+  console.log('Logging in user');
 
+  //get data from form
+  const { email, password } = req.body;
+
+  let errorMsg = {};
+
+  //validate data
+  let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;;
+  if (email.trim() == '')
+    errorMsg.email = 'Email is required';
+  else if (!email.match(emailFormat))
+    errorMsg.email = 'Invalid email';
+  else {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      errorMsg.email = "Email not found!";
+      return res.render('login', { errorMsg });
+    }
+  }
 
 export default router;
