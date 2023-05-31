6c06991bed359fb335b00ebf08dccbd8b098619d
Kidzantso <116034195+Kidzantso@users.noreply.github.com>
2023-05-31 18:22:32 +0300

    Working on checkout validation

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -138,7 +138,63 @@ const register = async (req, res) => {
   } 
 };
 
+const Checkout = async (req, res) => {
+console.log('Checkout');
+
+//get data from form
+const { fullname, email, address, cardnumber } = req.body;
+
+let errorMsg = {};
+
+//validate data
+if (fullname.trim() == '')
+  errorMsg.fullname = 'Full name is required';
+let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if (email.trim() == '')
+  errorMsg.email = 'Email is required';
+else if (!email.match(emailFormat))
+  errorMsg.email = 'Invalid email';
+if (address.trim() == '')
+  errorMsg.address = 'Address is required';
+
+let cdform = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+//Starting with 4 length 13 or 16 digits
+if (cardnumber.trim() == '')
+  errorMsg.cardnumber = 'cardnumber is required';
+else if (!cardnumber.match(cdform))
+  errorMsg.cardnumber = 'Invalid card';
+
+for (let key in errorMsg) {
+  console.log(errorMsg[key]);
+}
+
+if (Object.keys(errorMsg).length > 0) {
+  for (let key in errorMsg) {
+    console.log(errorMsg[key]);
+  }
+  if (req.query.json) {
+    console.log("Json errors");
+    return res.json( {errors: errorMsg, admin: false} );
+  }
+  else {
+    console.log("Not ajax errors");
+    return res.render("checkout", { errorMsg: errorMsg, admin: false });
+  }
+}
+else{
+  console.log("ADD");  
+  const user = await User.findOne({ _id: req.session.userID });
+  user.orders=user.cart;
+  user.cart=[];
+    await user.save();
+  console.log(user);
+  console.log("DONE Ordering");
+  res.redirect('Myproducts');
+}
+};
+
 export default {
   login,
   register,
+  Checkout,
 };
---
+++ public/JS/checkoutvalid.js
@@ -0,0 +1,92 @@
+class FormClass {
+    constructor(form) {
+        this.form = form;
+        this.noErr = true;
+    }
+
+    initialize() {
+        this.form.addEventListener("submit", (event) => {
+          console.log("Checkout user JS");
+          this.validateFields();
+          if (!this.noErr) {
+            event.preventDefault();
+          } else {
+            // Call the jQuery AJAX function
+            this.submitFormWithAjax(event);
+          }
+        });
+      }
+
+    resetErrors() {
+        let errors = document.getElementById("form").getElementsByClassName("errorMsg");
+        for (let i = 0; i < errors.length; i++) {
+            errors[i].innerHTML = "<br>";
+        }
+        this.noErr = true;
+    }
+    validateFields() {
+        this.resetErrors();
+        let fields = document.querySelectorAll("#form div input");
+
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].value = fields[i].value.trim();
+            let error = document.querySelectorAll("#form .errorMsg")[i];
+            if (fields[i].value === "") {
+                switch (fields[i].id) {
+                    case "fullname":
+                        error.innerHTML = "Name is required";
+                    break;
+                    case "address":
+                        error.innerHTML = "Address is required";
+                    break;
+                    case "email":
+                        error.innerHTML = "Email is required";
+                    break;
+                    case "cardnumber":
+                        error.innerHTML = "Cardnumber is required";
+                    break;
+                }
+                this.noErr = false;
+            }
+            else {
+                switch (fields[i].id) {
+                    case "Email":
+                        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                        if (!fields[i].value.match(mailformat)) {
+                            error.innerHTML = "Invalid Email";
+                            error.style.visibility = "visible";
+                            this.noErr = false;
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+
+    submitFormWithAjax(event) {
+        event.preventDefault();
+        const formData = $(this.form).serialize();
+        $.ajax({
+            url: "/register?ajax=true",
+            method: "POST",
+            data: formData,
+            success: function (response) {
+            if (Object.keys(response.errors).length === 0)
+                window.location.href = "/myproducts";
+            else {
+                $("#fullNameErrorMsg").html(response.errors.fullname);
+                $("#AdressErrorMsg").html(response.errors.address);
+                $("#emailErrorMsg").html(response.errors.email);
+                $("#CardErrorMsg").html(response.errors.cardnumber);
+            }
+            },
+            error: function (err) {
+            console.log(err);
+            },
+        });
+    }
+}
+
+const validator = new FormClass(document.getElementById("form"));
+validator.initialize();
