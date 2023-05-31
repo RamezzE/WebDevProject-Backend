dc6fde097d282cf71188b50c9e448830d93f9961
aya <aya2103436@miuegypt.edu.eg>
2023-06-01 00:22:44 +0300

    Hashing Password

--- models/user.js
+++ models/user.js
@@ -41,6 +41,25 @@ const userSchema = new mongoose.Schema({
   }
 ]
 });
+userSchema.pre('save', async function (next) {
+  try {
+  const salt = await bcrypt.genSalt(10);
+  const hashedpassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedpassword;
+  next();
+  } catch (error) {
+  next(error);
+  }
+  });
+  
+  
+  userSchema.methods.isValidPassword = async function (password) {
+  try {
+  return await bcrypt.compare(password, this.password);
+  } catch (error) {
+  throw error;
+  }
+  };
 
 const User = mongoose.model('User', userSchema);
 
